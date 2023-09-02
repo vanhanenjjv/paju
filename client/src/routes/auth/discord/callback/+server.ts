2f69b938b72f539a9ab9cd42dd22e6f5b4f5997a
Janne Vanhanen <janne.vanhanen@outlook.com>
2023-09-02 18:13:27 +0300

    Test

--- client/src/routes/auth/discord/callback/+server.ts
+++ client/src/routes/auth/discord/callback/+server.ts
@@ -1,5 +1,8 @@
 import type { RequestHandler } from "@sveltejs/kit";
+import { DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET } from "$env/static/private";
 
-export const POST: RequestHandler = async ({ request, platform }) => {
+export const POST: RequestHandler = async () => {
+  console.log(DISCORD_CLIENT_ID.length)
+  console.log(DISCORD_CLIENT_SECRET.length)
   return new Response("Hello world!")
 }
